3e56b9f4e3623988aa583491bfac6ed0e76027c9
salavatgibadullin <salavat9154@gmail.com>
2025-08-10 15:08:23 +0500

    start

---
+++ database.py
@@ -0,0 +1,31 @@
+from sqlalchemy	import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+DATABASE_URL = 'sqlite:///base.db'
+
+
+
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
+
+def create_db_and_tables():
+
+	Base.metadata.create_all(bind=engine)
+
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
+
+
+if __name__ == "__main__":
+	create_db_and_tables()
+	print("Database and tables created successfully.")
+
---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Users(Base):
+	__tablename__ = 'users'
+	user_id = Column(Integer, primary_key=True, index=True)
+	tg_id = Column(Integer, unique=True, index=True)
+	created_at = Column(String, nullable=False)
+	solved_total = Column(Integer, default=0)
+	streak_current = Column(Integer, default=0)
+	streak_best = Column(Integer, default=0)
+	last_task_date = Column(String, nullable=True)
+
+
+class Tasks(Base):
+	__tablename__ = 'tasks'
+	id = Column(Integer, primary_key=True, index=True)
+	date = Column(String, nullable=False, unique=False)
+	question = Column(String, nullable=False)
+	answer = Column(String, nullable=False)
+	hint = Column(String, nullable=True)
